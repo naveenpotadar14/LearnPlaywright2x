f72592df91eae62c82d5298867608462f57df9f7
naveenpotadar14 <naveenpotadar14@example.com>
2026-06-16 00:09:24 +0530

    Add chapter 5 literals - null, string, template literals, and number literals

---
+++ chapter_05_Literals/24_null.js
@@ -0,0 +1,10 @@
+let naveen_do_not_have_car = null;
+let pramod_has_two_wives;
+
+let age = 0;
+
+let skoda = "";
+console.log(typeof skoda);
+console.log(typeof age);
+console.log(typeof naveen_do_not_have_car);
+console.log(typeof pramod_has_two_wives);
